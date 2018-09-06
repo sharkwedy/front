ac70749c16dc511222a5ede7ade9dc7cf8effe0c
sharkwedy <talls.urin@hotmail.com>
2018-09-06 09:00:18 -0300

    f

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.set('port', process.env.PORT || 4200);
 
 app.use(express.static('./'));
 
-app.get('/*', function(req,res) {
+app.get('/', function(req,res) {
 
 res.sendFile(path.join('./index.html'));
 });
