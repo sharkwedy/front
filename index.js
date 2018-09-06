30ce15ba9819be451f584a2d6ba29971f6189fec
sharkwedy <talls.urin@hotmail.com>
2018-09-06 16:01:05 -0300

    f

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-var shell = require('shelljs');
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/frontend'));
 
-shell.exec('ng serve --'+process.env.PORT).code;
+app.get('/*', function(req,res) {
+
+res.sendFile(path.join(__dirname+'/dist/frontend/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 4200);
