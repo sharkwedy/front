45d289cb5536811f1205ee56bec62fe9fea541b6
sharkwedy <talls.urin@hotmail.com>
2018-09-06 07:54:27 -0300

    f

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const app = express();
 // Settings
 app.set('port', process.env.PORT || 4200);
 
+app.use(express.static(__dirname + '/dist/frontend'));
+
+app.get('/*', function(req,res) {
+
+res.sendFile(path.join(__dirname+'/dist/frontend/index.html'));
+});
+
 // Starting the server
 app.listen(app.get('port'), () => {
     console.log('Server on port', app.get('port'));
