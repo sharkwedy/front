24660388e05cb75f39f64efe29939989c3b8d568
sharkwedy <talls.urin@hotmail.com>
2018-09-06 16:09:42 -0300

    f

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/frontend'));
+//app.use(express.static(__dirname + '/dist/frontend'));
 
-app.get('/*', function(req,res) {
+app.get('/', function(req,res) {
 
-res.sendFile(path.join('/src/index.html'));
+//res.sendFile(path.join('/src/index.html'));
+res.send('/src/index.html');
 });
 
 // Start the app by listening on the default Heroku port
